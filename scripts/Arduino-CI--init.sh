86374ae26d5d469d2150c2d64460d31358bdcdde
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-03 16:49:29 +0100

    Updated repo names + removed non-compliant external libraries from unittest

--- scripts/Arduino-CI--init.sh
+++ scripts/Arduino-CI--init.sh
@@ -4,7 +4,11 @@
 set -e
 
 # pull in external repositories
-git clone https://github.com/s-t-a-n/Structures s-t-a-n--Structures
+git clone https://github.com/autonomousrobotshq/External--Structures Structures
+git clone https://github.com/autonomousrobotshq/External--TinyGPS TinyGPS
+git clone https://github.com/autonomousrobotshq/External--LSM303 LSM303
+git clone https://github.com/autonomousrobotshq/External--OneWire OneWire
+git clone https://github.com/autonomousrobotshq/External--DallasTemperature DallasTemperature
 
 # pull in internal repositories
 git clone https://github.com/autonomousrobotshq/Spine--Sensors
@@ -12,7 +16,3 @@ git clone https://github.com/autonomousrobotshq/Spine--Actuators
 git clone https://github.com/autonomousrobotshq/Spine--Filters
 git clone https://github.com/autonomousrobotshq/Spine--Timers
 git clone https://github.com/autonomousrobotshq/Spine--ROS-Generated
-git clone https://github.com/autonomousrobotshq/TinyGPS
-git clone https://github.com/autonomousrobotshq/LSM303
-git clone https://github.com/autonomousrobotshq/OneWire
-git clone https://github.com/autonomousrobotshq/DallasTemperature
